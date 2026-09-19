cfde42ce3e02bb37b0fbe8e1cb0d05620d172e08
myhomerico-stack <myhomerico@gmail.com>
2026-09-19 15:46:54 +0200

    Opret På farten Android grundprojekt

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "dk.fynogjylland.paafarten"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "dk.fynogjylland.paafarten"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildFeatures { compose = true }
+
+    packaging {
+        resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+}
+
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2025.08.01"))
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
