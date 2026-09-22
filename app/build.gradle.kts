5a5173f749eae50bcac36cd69b2842d2c3346284
myhomerico-stack <myhomerico@gmail.com>
2026-09-22 03:08:59 +0200

    Bump version til 0.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dk.fynogjylland.paafarten"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.3"
+        versionCode = 6
+        versionName = "0.2.4"
     }
 
     compileOptions {
